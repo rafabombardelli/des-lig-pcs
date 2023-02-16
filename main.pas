3000914ff9006ee5e1c0fddda8d37ef29e251a94
unknown <rafaelgaiobombardelli@gmail.com>
2023-02-16 08:19:07 -0300

    correção

--- main.pas
+++ main.pas
@@ -39,7 +39,7 @@ implementation
 procedure Ttl_principal.dest_btnClick(Sender: TObject);
 begin
   Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_todos.bat';
-  ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+  ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
 end;
 
 procedure Ttl_principal.des_btnClick(Sender: TObject);
@@ -47,99 +47,99 @@ begin
 if RadioGroup.ItemIndex = 0 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_A1.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 1 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_A2.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 2 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_A3.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 3 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_A4.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 4 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_A5.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 5 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_A6.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 6 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_A7.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 7 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_B1.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 8 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_B2.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 9 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_B3.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 10 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_B4.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 11 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_B5.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 12 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_B6.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 13 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_B7.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 14 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_B8.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 15 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_C3.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 16 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_C4.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 17 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\des_C5.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 end;
 
 procedure Ttl_principal.ligt_btnClick(Sender: TObject);
 begin
   Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_todos.bat';
-  ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+  ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
 end;
 
 procedure Ttl_principal.lig_btnClick(Sender: TObject);
@@ -147,92 +147,92 @@ begin
 if RadioGroup.ItemIndex = 0 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_A1.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 1 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_A2.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 2 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_A3.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 3 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_A4.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 4 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_A5.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 5 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_A6.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 6 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_A7.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 7 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_B1.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 8 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_B2.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 9 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_B3.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 10 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_B4.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 11 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_B5.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 12 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_B6.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 13 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_B7.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 14 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_B8.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 15 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_C3.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 16 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_C4.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 else if RadioGroup.ItemIndex = 17 then
   begin
     Arquivo := 'C:\Program Files (x86)\Ligar-Desligar_PCs\Comandos\cmd\lig_C5.bat';
-    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_SHOWNORMAL);
+    ShellExecute(0, 'open', PChar(Arquivo), nil, nil, SW_HIDE);
   end
 end;
 procedure Ttl_principal.FormCreate(Sender: TObject);
